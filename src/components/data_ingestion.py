9a6b987c9a34383ddcc600c827b9213aa2778261
Faldho23 <faldho.wirianandaho@gmail.com>
2025-09-05 18:05:27 +0800

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', 'train.csv')
@@ -47,4 +49,7 @@ if __name__=="__main__":
     train_path, test_path=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_path, test_path)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_path, test_path)
+
+    model_trainer=ModelTrainer()
+    print("Best model R2: " + str(model_trainer.initiate_model_trainer(train_arr, test_arr)))
